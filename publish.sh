a997a356d2bcedc6686b6c064b4ab88602485717
mnmly <i.am@mnmly.com>
2013-01-02 14:51:46 -0800

    Add message

--- publish.sh
+++ publish.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-# Push source branch
-git checkout source
-git add -A
-git commit
-git push origin source
-
 clear
 
 echo '  '
@@ -16,6 +10,12 @@ echo '  '
 echo '  '
 echo '  '
 echo '  '
+
+# Push source branch
+git checkout source
+git add -A
+git commit
+git push origin source
 
 
 # Push master branch
@@ -43,5 +43,5 @@ git checkout source
 
 clear
 echo ''
-echo  "\033[0;36m ♔ Sweetness ♔ \033[0m"
+echo  "\t\033[0;36m ⚐  \033[1;36mSweetness\033[0;36m\033[0m"
 echo '  '
