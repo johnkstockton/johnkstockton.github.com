3ea126ec93eb2186bd450a068fe70eb207bd060f
mnmly <i.am@mnmly.com>
2013-01-02 14:39:48 -0800

    update

--- publish.sh
+++ publish.sh
@@ -5,6 +5,19 @@ git add -A
 git commit
 git push origin source
 
+clear
+
+echo '  '
+echo '  '
+echo '  '
+echo '  '
+echo  "\033[0;36mPushing source to github\033[0m"
+echo '  '
+echo '  '
+echo '  '
+echo '  '
+
+
 # Push master branch
 jekyll
 git checkout master
@@ -12,5 +25,18 @@ git rm -qr .
 cp -r _site/. .
 rm -r _site
 git add -A
-git commit
+git commit 'update'
 git push origin master
+
+clear
+
+echo '  '
+echo '  '
+echo '  '
+echo '  '
+echo 'Published successfully.'
+echo '  '
+echo '  '
+echo '  '
+echo '  '
+
